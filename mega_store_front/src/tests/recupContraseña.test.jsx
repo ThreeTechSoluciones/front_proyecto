61fc087947c6e66d47a36344af37cda84de0e5a8
AbrilConrero <abrilconrero@gmail.com>
2025-05-02 21:45:46 -0300

    Agregué para testear con Jest

---
+++ mega_store_front/src/tests/recupContraseña.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import Login from '../pages/login/login'
+import '@testing-library/jest-dom';
+import React from "react";
+import { AuthProvider } from "../contexts/LoginContext";
+import { MemoryRouter } from "react-router-dom";
+import { CarritoProvider } from "../contexts/CarritoContext";
+describe ('RecupContraseña component', ()=>{
+    test ('El enlace de recuperación se encuentra en el documento', ()=>{
+        render(
+            <MemoryRouter>
+              <CarritoProvider>
+                <AuthProvider>
+                  <Login />
+                </AuthProvider>
+              </CarritoProvider>
+            </MemoryRouter>
+          );
+    const linkRecuperacion = screen.getByRole('link', {name:'¿Olvidaste tu contraseña?'});
+    expect (linkRecuperacion).toBeInTheDocument();
+    });
+});
