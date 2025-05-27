a028a1ef377574b41e8200427294a67378e08cce
AbrilConrero <abrilconrero@gmail.com>
2025-05-27 17:40:18 -0300

    WIP

--- mega_store_front/src/tests/categoria.test.jsx
+++ mega_store_front/src/tests/categoria.test.jsx
@@ -12,7 +12,29 @@ import {
   useCategoriaContext,
 } from "../contexts/CategoriaContext";
 import "@testing-library/jest-dom";
-import { ToastContainer } from "react-toastify";
+import { toast } from 'react-toastify';
+
+// MOCKS AL INICIO
+
+// Mock de react-toastify
+jest.mock('react-toastify', () => ({
+  toast: {
+    success: jest.fn(),
+    error: jest.fn(),
+    info: jest.fn(),
+    warn: jest.fn(),
+  },
+  ToastContainer: () => <div />,
+}));
+
+// Mock de la función que hace el POST
+jest.mock('../service/CategoriaService', () => ({
+  newCategoria: jest.fn().mockResolvedValue({
+    data: { nombre: 'L' },
+  }),
+}));
+
+
 
 describe("Categoría component", () => {
   test("U2-006: Validar longitud de campo.", async () => {
@@ -44,4 +66,26 @@ describe("Categoría component", () => {
       await screen.findByText("Categoría no puede estar vacío.")
     ).toBeInTheDocument();
   });
+ 
+
+test("U2-003: Mostrar mensaje de éxito al registrar categoría nueva", async () => {
+  render(
+    <CategoriaProvider>
+      <RegistrarCategoria />
+    </CategoriaProvider>
+  );
+
+  const input = screen.getByPlaceholderText("Categoría");
+  const button = screen.getByRole("button", { name: "Registrar" });
+
+  fireEvent.change(input, { target: { value: "L" } });
+  fireEvent.click(button);
+
+  await waitFor(() =>
+    expect(toast.success).toHaveBeenCalledWith(
+      "Categoria L registrada",
+      expect.any(Object)
+    )
+  );
+});
 });
