feaaa72a078e08cbf12fee1d2d267191fa663f97
RocioMenardi <romenardi08@gmail.com>
2025-05-27 17:04:43 -0300

    Tests unitarios - Front - Vaidar nombre Perfil
    Se realizaron dos test unitarios para validar las funciones de validaciones sobre el nombre obligatorio (no puede estar vacío) y sobre que no puede haber dos espacios consecutivos.

---
+++ mega_store_front/src/tests/testsUnitarios/validarNombrePerfil.test.jsx
@@ -0,0 +1,30 @@
+import { validationsPerfil } from "../../pages/perfil/validationsPerfil";
+
+describe("validationsPerfil - nombre", () => {
+  const schema = validationsPerfil;
+
+  const baseData = {
+    apellido: "Martinez",
+    email: "luciana@email.com",
+    direccionEnvio: "calle 123",
+    numeroTelefono: "12345678",
+  };
+
+  //test unitario para la validación de campo obligatorio
+  test("rechaza nombre vacío", () => {
+    const result = schema.safeParse({ nombre: "" });
+    expect(result.success).toBe(false);
+    expect(result.error?.format().nombre?._errors[0]).toBe(
+      "Nombre no puede estar vacío."
+    );
+  });
+
+  //test unitario para la validación del campo nombre en donde se rechazan los espacios dobles
+  test("rechaza nombre con espacios dobles", () => {
+    const result = schema.safeParse({ nombre: "Luciana  Martinez" });
+    expect(result.success).toBe(false);
+    expect(result.error?.format().nombre?._errors[0]).toBe(
+      "Nombre no puede contener espacios consecutivos"
+    );
+  });
+});
